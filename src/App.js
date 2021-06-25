8f0bc1f0e5d3db4f62ff531a2f279a0a65e183d6
Liridona <liridonaosm@gmail.com>
2021-06-25 09:47:55 +0200

    fixed the code joâo gave me

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from "react";
 import Weather from "./weather";
-
-
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1> Hello from React</h1>
-       <Weather city="Oslo" />
-
-          <footer>
-          <small>
-            <a href="https://github.com/lirosm/Vanilla-weather-app" target="_blank">
-              Open-source code</a>
-            , by Liriona Osmani
-          </small>
-        </footer>
+        <h1> Hello from React</h1>
       </header>
+      <Weather city="Oslo" />
     </div>
   );
 }
-
 export default App;
--- src/weather.js
+++ src/weather.js
@@ -1,158 +1,89 @@
 import React from "react";
-import Axios from "axios";
-import App from "./App";
-
-
-
-
-function formatDate(timestamp) {
-let date = new Date (timestamp);
-let hours = date.getHours();
-if (hours < 10) {
-hours = `0${hours}`;
-}
-
-let minutes = date.getMinutes();
-if (minutes < 10){
-minutes = `0${minutes}`;
-}
-
-let days = [
-     "Sunday",
-     "Monday", 
-     "Tuesday", 
-     "Wednesday", 
-     "Thursday", 
-     "Friday", 
-     "Saturday"
-    ];
-
-let day = days [date.getDay()];
-
-return `${day} ${hours}:${minutes}`;
-}
-
-
-function formatDay(timestamp) {
-let date = new Date(timestamp * 1000);
-let day = date.getDay();
-let days = [
-     "SUN",
-     "MON", 
-     "TUE", 
-     "WED", 
-     "THU", 
-     "FRI", 
-     "SAT"
-    ];
-
-
-return days[day];
-
-}
-
-
-function displayForecast(response) {
-
-    let forecast= response.data.daily;
-
-    let forecastElement = document.querySelector("#Forecast");
-
-
-       let forecastHTML = `<div class="row">`;
-     
-       forecast.forEach(function(forecastDay, index) {
-         if (index < 6) {
-         forecastHTML = forecastHTML +
-      `
-      <div class="col-2">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-        <img
-          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
-          alt=""
-          width="42"
-        />
-        <div class="weather-forecast-temp">
-          <span class="weather-forecast-temp-max"> ${Math.round (forecastDay.temp.max)}°C |</span>
-          <span class="weather-forecast-temp-min"> ${Math.round(forecastDay.temp.min)}°C </span>
+export default function Weather() {
+  return (
+    <div className="container">
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
+          <form id="search-form" className="mb-3">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="City..."
+                  className="form-control"
+                  id="city-input"
+                  autocomplete="off"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="search"
+                  className="btn btn-primary w-100"
+                />
+              </div>
+            </div>
+          </form>
+          <div className="center">
+            <div className="row">
+              <div className="col-4">
+                <div className="overview">
+                  <h1 id="city">Oslo</h1>
+                  <ul>
+                    <li>
+                      updated: <span id="date"></span>
+                    </li>
+                    <li id="description"></li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-4">
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+                  alt="weather-icon"
+                  id="icon"
+                  className="float-left"
+                />
+              </div>
+              <div className="col-4">
+                <div className="d-flex weather-temp justify-content-center">
+                  <div className="float-right">
+                    <span id="temperature">16</span>
+                    <span className="units">°C</span>
+                  </div>
+                </div>
+                <div className="d-flex justify-content-center">
+                  <ul>
+                    <li>
+                      Feels like: <span id="feels-like">15</span>°C
+                    </li>
+                    <li>
+                      Humidity:<span id="humidity">27</span>%
+                    </li>
+                    <li>
+                      Wind:<span id="wind">2</span> Km/H
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="weather-forecast" id="Forecast"></div>
+          <footer>
+            <small>
+              <a
+                href="https://github.com/lirosm/Vanilla-weather-app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Open-source code
+              </a>
+              , by Liriona Osmani
+            </small>
+          </footer>
         </div>
       </div>
-  `;    
-         }
-       });
-  
-  
-
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
- 
+    </div>
+  );
 }
-
-function getForecast(coordinates){
- 
-
-    let apiKey = "c898564bae75feba5b1af523cf4a1211";
-
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
-
-    axios.get(apiUrl).then(displayForecast);
-}
-
-
-
-function displayTemp(response){
-
-let temperatureElement = document.querySelector("#temperature");
-let cityElement = document.querySelector("#city");
-let descriptionElement = document.querySelector("#description");
-let humidityElement = document.querySelector("#humidity");
-let feelsLikeElement = document.querySelector("#feels-like");
-let windElement = document.querySelector("#wind");
-let dateElement = document.querySelector("#date");
-let iconElement = document.querySelector("#icon");
-
-
-
-celsiusTemp = response.data.main.temp;
-
-
-
-temperatureElement.innerHTML = Math.round (celsiusTemp);
-cityElement.innerHTML = response.data.name;
-descriptionElement.innerHTML = response.data.weather[0].description;
-humidityElement.innerHTML = response.data.main.humidity;
-feelsLikeElement.innerHTML = Math.round (response.data.main.feels_like);
-windElement.innerHTML = Math.round (response.data.wind.speed);
-dateElement.innerHTML = formatDate(response.data.dt * 1000);
-iconElement.setAttribute("src", 
-`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-iconElement.setAttribute("alt", response.data.weather[0].description);
-
-getForecast(response.data.coord);
-
-}
-
-
-function search(city){
-    
-let apiKey = "c898564bae75feba5b1af523cf4a1211";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemp);
-
-}
-
-function handleSubmit(event) {
-event.preventDefault();
-let cityInputElement = document.querySelector("#city-input");
-search(cityInputElement.value);
-}
-
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
-
-
-search("Oslo");
-
-
